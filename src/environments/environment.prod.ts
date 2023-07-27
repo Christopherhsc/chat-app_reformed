c940a9cc3300a609ff625e6165a97db836fddf1b
Christopherhsc <Christopherhsc2520@gmail.com>
2023-07-27 03:02:29 +0200

    project now setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-app-83d81',
+    appId: '1:750088505481:web:ffe86010c6eca1d02e5e58',
+    storageBucket: 'angular-app-83d81.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyC_7Nl2thgSj7q6_sLHJowOpZR7HSXMx8s',
+    authDomain: 'angular-app-83d81.firebaseapp.com',
+    messagingSenderId: '750088505481',
+  },
   production: true
 };
